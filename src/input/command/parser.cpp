9c35353dc47dbbfe294615bcbb8408070027be47
OswalB <gafitexpasto@gmail.com>
2026-02-24 15:19:47 -0500

    command  resolver basico

--- src/input/command/parser.cpp
+++ src/input/command/parser.cpp
@@ -1,6 +1,69 @@
 # include "parser.h"
 #include "system/console/console.h"
+#include "lib/utils/utils.h"
+#include "core/command/command_resolver.h"
 
+#define MAX_TOKEN_LENGTH 32
+#define MAX_TOKENS 8
+
+static int tokenize(const char *line, char tokens[][MAX_TOKEN_LENGTH]);
+
+// --------------------------------------------
+// Parser principal
+// --------------------------------------------
 void parser_parse(const char *line){
-    Console_Print(MSG_DBG,"parserizando ...");
+    char tokens[MAX_TOKENS][MAX_TOKEN_LENGTH];
+    int token_count = tokenize(line, tokens);
+
+    if (token_count == 0)
+        return;
+
+    for (int i = 0; i < token_count; i++)
+    {
+        if (!utils_is_numeric(tokens[i]))
+        {
+            utils_to_uppercase(tokens[i]);
+        }
+    }
+
+    CommandMatch match;
+
+    if (!command_resolve(tokens, token_count, &match))
+    {
+        Console_Print(MSG_LOG,"ERR: Unknown command\n");
+        return;
+    }
+
+    Console_Print(MSG_DBG,"parserizando ...%s",tokens[0]);
+
+}
+
+// --------------------------------------------
+// Tokenizador simple
+// --------------------------------------------
+static int tokenize(const char *line, char tokens[][MAX_TOKEN_LENGTH])
+{
+    int count = 0;
+
+    const char *ptr = line;
+
+    while (*ptr != '\0' && count < MAX_TOKENS)
+    {
+        while (*ptr == ' ')
+            ptr++;
+
+        if (*ptr == '\0')
+            break;
+
+        int len = 0;
+        while (*ptr != ' ' && *ptr != '\0' && len < MAX_TOKEN_LENGTH - 1)
+        {
+            tokens[count][len++] = *ptr++;
+        }
+
+        tokens[count][len] = '\0';
+        count++;
+    }
+
+    return count;
 }
