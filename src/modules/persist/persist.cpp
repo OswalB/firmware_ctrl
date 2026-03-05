85224428df35d02f6c2ca6145137a4ad1e9875e8
OswalB <grafitexpasto@gmail.com>
2026-03-05 08:44:51 -0500

    refact persist fail

--- src/modules/persist/persist.cpp
+++ src/modules/persist/persist.cpp
@@ -3,13 +3,13 @@
 #include "../console/console.h"
 
 #define EEPROM_TOTAL_BYTES 4096
-#define EEPROM_SLOT_SIZE   (EEPROM_TOTAL_BYTES / 2)
+#define EEPROM_SLOT_SIZE (EEPROM_TOTAL_BYTES / 2)
 
-#define CONFIG_MAGIC   0xA55A5AA5
+#define CONFIG_MAGIC 0xA55A5AA5
 #define CONFIG_VERSION 0x0001
 
-#define EEPROM_SLOT_A_ADDR  0
-#define EEPROM_SLOT_B_ADDR  EEPROM_SLOT_SIZE
+#define EEPROM_SLOT_A_ADDR 0
+#define EEPROM_SLOT_B_ADDR (EEPROM_SLOT_A_ADDR + EEPROM_SLOT_SIZE)
 
 typedef struct
 {
@@ -19,7 +19,6 @@ typedef struct
     uint32_t generation;
 
 } ConfigHeader;
-
 
 typedef struct
 {
@@ -31,28 +30,128 @@ typedef struct
 
 static MachineConfig g_config;
 
-
-
 static_assert(sizeof(ConfigSlot) <= EEPROM_SLOT_SIZE,
               "ConfigSlot exceeds EEPROM slot size");
 
+static uint32_t g_generation = 0;
 
 //------ FORWARDS -------
-static bool persist_load_slotA(void);
-
+// static bool persist_load_slot(uint16_t addr, ConfigSlot *out_slot);
+static uint32_t crc32_compute(const uint8_t *data, size_t length);
+static bool persist_validate_slot(const ConfigSlot *slot);
+static bool persist_read_slot(uint16_t addr, ConfigSlot *slot);
+static bool persist_write_slot(uint16_t addr);
 // ----------------------
 
 bool persist_init(void)
 {
     Console_Print(MSG_LOG, "*** Persist module init ***");
-    if (!persist_load_slotA())
+
+    ConfigSlot slotA;
+    ConfigSlot slotB;
+
+    bool validA = persist_read_slot(EEPROM_SLOT_A_ADDR, &slotA);
+    bool validB = persist_read_slot(EEPROM_SLOT_B_ADDR, &slotB);
+
+    if (validA)
     {
-        Console_Print(MSG_LOG, "Loading defaults...");
-        persist_load_defaults();
-        Console_Print(MSG_LOG, "Initializing EEPROM slot...");
-        persist_save_slotA();
+        g_config = slotA.config;
+        g_generation = slotA.header.generation;
+
+        Console_Print(MSG_LOG, "Slot A loaded");
+        return true;
     }
+
+    if (validB)
+    {
+        g_config = slotB.config;
+        g_generation = slotB.header.generation;
+
+        Console_Print(MSG_LOG, "Slot B loaded");
+        return true;
+
+        Console_Print(MSG_LOG, "Stored CRC: %08X", header->crc);
+        Console_Print(MSG_LOG, "Calc CRC:   %08X", crc_check);
+    }
+
+    Console_Print(MSG_LOG, "No valid slots");
+
+    persist_load_defaults();
+    persist_write_slot(EEPROM_SLOT_A_ADDR);
+
     return true;
+}
+
+static void persist_build_slot(ConfigSlot *slot)
+{
+    slot->header.magic = CONFIG_MAGIC;
+    slot->header.version = CONFIG_VERSION;
+    slot->header.size = sizeof(ConfigSlot);
+    slot->header.generation = ++g_generation;
+
+    slot->config = g_config;
+    /*
+        size_t crc_length = sizeof(ConfigHeader) + sizeof(MachineConfig);
+
+        slot->crc = crc32_compute(
+            (uint8_t*)slot,
+            crc_length
+        );*/
+    slot->crc = crc32_compute(
+        (uint8_t *)slot,
+        sizeof(ConfigSlot) - sizeof(uint32_t));
+}
+
+static bool persist_read_slot(uint16_t addr, ConfigSlot *slot)
+{
+    eeprom_read_block(
+        addr,
+        (uint8_t *)slot,
+        sizeof(ConfigSlot));
+
+    return persist_validate_slot(slot);
+}
+
+static bool persist_write_slot(uint16_t addr)
+{
+    ConfigSlot slot;
+
+    persist_build_slot(&slot);
+
+    Console_Print(MSG_LOG, "Writing slot @ %u", addr);
+
+    eeprom_write_block(
+        addr,
+        (uint8_t *)&slot,
+        sizeof(ConfigSlot));
+
+    ConfigSlot verify;
+
+    eeprom_read_block(
+        addr,
+        (uint8_t *)&verify,
+        sizeof(ConfigSlot));
+
+    /*size_t crc_length = sizeof(ConfigHeader) + sizeof(MachineConfig);
+
+    uint32_t crc_check = crc32_compute(
+        (uint8_t*)&verify,
+        crc_length
+    );*/
+    size_t crc_length = sizeof(ConfigSlot) - sizeof(uint32_t);
+
+    uint32_t crc_check = crc32_compute(
+        (const uint8_t *)&verify,
+        crc_length);
+
+    if (crc_check == verify.crc)
+    {
+        Console_Print(MSG_LOG, "Verification OK");
+        return true;
+    }
+
+    Console_Print(MSG_ERR, "Verification FAILED");
+    return false;
 }
 
 size_t persist_get_slot_size(void)
@@ -70,7 +169,7 @@ size_t persist_get_remaining_bytes(void)
     return EEPROM_SLOT_SIZE - sizeof(ConfigSlot);
 }
 
-MachineConfig* persist_get_config(void)
+MachineConfig *persist_get_config(void)
 {
     return &g_config;
 }
@@ -94,7 +193,7 @@ void persist_load_defaults(void)
     Console_Print(MSG_LOG, "Defaults loaded into RAM config");
 }
 
-static uint32_t crc32_compute(const uint8_t* data, size_t length)
+static uint32_t crc32_compute(const uint8_t *data, size_t length)
 {
     uint32_t crc = 0xFFFFFFFF;
 
@@ -114,34 +213,7 @@ static uint32_t crc32_compute(const uint8_t* data, size_t length)
     return ~crc;
 }
 
-static uint32_t g_generation = 0;
-
-void persist_debug_build_slot(void)
-{
-    ConfigSlot slot;
-
-    slot.header.magic = CONFIG_MAGIC;
-    slot.header.version = CONFIG_VERSION;
-    slot.header.size = sizeof(ConfigSlot);
-    slot.header.generation = ++g_generation;
-
-    // Copiar config activa
-    slot.config = g_config;
-
-    // Calcular CRC sobre header + config
-    size_t crc_length = sizeof(ConfigHeader) + sizeof(MachineConfig);
-
-    slot.crc = crc32_compute(
-        (uint8_t*)&slot,
-        crc_length
-    );
-
-    Console_Print(MSG_LOG, "Slot built");
-    Console_Print(MSG_LOG, "Generation: %lu", slot.header.generation);
-    Console_Print(MSG_LOG, "CRC: 0x%08lX", slot.crc);
-}
-
-static void eeprom_write_block(uint16_t addr, const uint8_t* data, size_t len)
+static void eeprom_write_block(uint16_t addr, const uint8_t *data, size_t len)
 {
     for (size_t i = 0; i < len; i++)
     {
@@ -149,7 +221,7 @@ static void eeprom_write_block(uint16_t addr, const uint8_t* data, size_t len)
     }
 }
 
-static void eeprom_read_block(uint16_t addr, uint8_t* data, size_t len)
+static void eeprom_read_block(uint16_t addr, uint8_t *data, size_t len)
 {
     for (size_t i = 0; i < len; i++)
     {
@@ -159,49 +231,15 @@ static void eeprom_read_block(uint16_t addr, uint8_t* data, size_t len)
 
 void persist_save_slotA(void)
 {
-    ConfigSlot slot;
-
-    slot.header.magic = CONFIG_MAGIC;
-    slot.header.version = CONFIG_VERSION;
-    slot.header.size = sizeof(ConfigSlot);
-    slot.header.generation = ++g_generation;
-
-    slot.config = g_config;
-
-    size_t crc_length = sizeof(ConfigHeader) + sizeof(MachineConfig);
-
-    slot.crc = crc32_compute((uint8_t*)&slot, crc_length);
-
-    Console_Print(MSG_LOG, "Writing Slot A...");
-
-    eeprom_write_block(
-        EEPROM_SLOT_A_ADDR,
-        (uint8_t*)&slot,
-        sizeof(ConfigSlot)
-    );
-
-    Console_Print(MSG_LOG, "Write done. Verifying...");
-
-    // Verificación
-    ConfigSlot verify;
-    eeprom_read_block(
-        EEPROM_SLOT_A_ADDR,
-        (uint8_t*)&verify,
-        sizeof(ConfigSlot)
-    );
-
-    uint32_t crc_check = crc32_compute(
-        (uint8_t*)&verify,
-        crc_length
-    );
-
-    if (crc_check == verify.crc)
-        Console_Print(MSG_LOG, "Verification OK");
-    else
-        Console_Print(MSG_ERR, "Verification FAILED");
+    persist_write_slot(EEPROM_SLOT_A_ADDR);
 }
 
-static bool persist_validate_slot(const ConfigSlot* slot)
+void persist_save_slotB(void)
+{
+    persist_write_slot(EEPROM_SLOT_B_ADDR);
+}
+
+static bool persist_validate_slot(const ConfigSlot *slot)
 {
     if (slot->header.magic != CONFIG_MAGIC)
         return false;
@@ -212,40 +250,20 @@ static bool persist_validate_slot(const ConfigSlot* slot)
     if (slot->header.size != sizeof(ConfigSlot))
         return false;
 
-    size_t crc_length = sizeof(ConfigHeader) + sizeof(MachineConfig);
+    /*size_t crc_length = sizeof(ConfigHeader) + sizeof(MachineConfig);
 
     uint32_t crc_check = crc32_compute(
-        (const uint8_t*)slot,
-        crc_length
-    );
+        (const uint8_t *)slot,
+        crc_length);*/
+
+    size_t crc_length = sizeof(ConfigSlot) - sizeof(uint32_t);
+
+    uint32_t crc_check = crc32_compute(
+        (const uint8_t *)slot,
+        crc_length);
 
     if (crc_check != slot->crc)
         return false;
-
-    return true;
-}
-
-static bool persist_load_slotA(void)
-{
-    ConfigSlot slot;
-
-    eeprom_read_block(
-        EEPROM_SLOT_A_ADDR,
-        (uint8_t*)&slot,
-        sizeof(ConfigSlot)
-    );
-
-    if (!persist_validate_slot(&slot))
-    {
-        Console_Print(MSG_LOG, "Slot A invalid");
-        return false;
-    }
-
-    g_config = slot.config;
-    g_generation = slot.header.generation;
-
-    Console_Print(MSG_LOG, "Slot A loaded");
-    Console_Print(MSG_LOG, "Generation: %lu", g_generation);
 
     return true;
 }
