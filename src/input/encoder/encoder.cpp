f0e457edc6e0f1e61c452a04209f6e7d02faf562
OswalB <grafitexpasto@gmail.com>
2026-04-07 22:43:15 -0500

    Rotary Encoder lib

--- src/input/encoder/encoder.cpp
+++ src/input/encoder/encoder.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <RotaryEncoder.h>
 #include "encoder.h"
 #include "core/event/event_queue.h"
 #include "core/model/key_types.h"
@@ -6,18 +7,18 @@
 #include "modules/console/console.h"
 
 // Pines Arduino Mega
-#define ENC_A_PIN 2  // CLK
-#define ENC_B_PIN 3  // DT
-//#define ENC_SW_PIN 4 // botón
+#define ENC_A_PIN 2 // CLK
+#define ENC_B_PIN 3 // DT
 
-//#define LONG_PRESS_MS 800
+static long lastPos = 0;
 
-//static uint32_t press_time = 0;
-//static uint8_t long_sent = 0;
+static RotaryEncoder encoder(
+    ENC_A_PIN,
+    ENC_B_PIN,
+    RotaryEncoder::LatchMode::TWO03);
 
-static uint8_t prev_state = 0;
-static int8_t acc = 0;
-//static uint8_t last_btn = 1;
+// static uint8_t prev_state = 0;
+// static int8_t acc = 0;
 
 static uint8_t read_encoder(void)
 {
@@ -28,11 +29,7 @@ static uint8_t read_encoder(void)
 
 void encoder_init(void)
 {
-    pinMode(ENC_A_PIN, INPUT_PULLUP);
-    pinMode(ENC_B_PIN, INPUT_PULLUP);
-    //pinMode(ENC_SW_PIN, INPUT_PULLUP);
-
-    prev_state = read_encoder();
+    encoder.setPosition(0);
 }
 
 void encoder_update(void)
@@ -42,82 +39,16 @@ void encoder_update(void)
 
     bool send = false;
 
-    // BOTÓN
-    //uint8_t btn = digitalRead(ENC_SW_PIN);
-    //uint32_t now = millis();
+    encoder.tick();
+    long pos = encoder.getPosition();
 
-    // Detectar PRESIÓN
-    /*if (btn == 0 && last_btn == 1)
+    if (pos != lastPos)
     {
-        press_time = now;
-        long_sent = 0;
-    }*/
-
-    // Detectar MANTENIDO (long press)
-    /*if (btn == 0 && !long_sent)
-    {
-        if ((now - press_time) > LONG_PRESS_MS)
-        {
-            ev.value = KEY_LONG;
-            send = true;
-
-            long_sent = 1; // evitar repetición
-        }
-    }*/
-
-    // Detectar LIBERACIÓN (short press)
-    /*if (btn == 1 && last_btn == 0)
-    {
-        if (!long_sent)
-        {
-            ev.value = KEY_OK;
-            send = true;
-        }
+        ev.value = pos > lastPos ? KEY_DOWN : KEY_UP;
+        send = true; 
+        lastPos = pos;
     }
 
-    last_btn = btn;*/
-
-    // ENCODER
-    uint8_t state = read_encoder();
-
-    if (state != prev_state)
-    {
-
-        // Decodificación Gray
-        if (
-            (prev_state == 0b00 && state == 0b01) ||
-            (prev_state == 0b01 && state == 0b11) ||
-            (prev_state == 0b11 && state == 0b10) ||
-            (prev_state == 0b10 && state == 0b00))
-        {
-            acc++;
-        }
-        else
-        {
-            acc--;
-        }
-
-        // KY-040 → 4 pasos por "click"
-        if (state == 0b00)
-        {
-            if (acc > 0)
-            {
-                //Console_Print(MSG_DBG, "DOWN");
-                ev.value = KEY_DOWN;
-                send = true;
-            }
-            else if (acc < 0)
-            {
-                //Console_Print(MSG_DBG, "UP");
-                ev.value = KEY_UP;
-                send = true;
-            }
-
-            acc = 0; // reset para siguiente click
-        }
-
-        prev_state = state;
-    }
 
     // ---------------- ENVÍO EVENTO ----------------
     if (send)
